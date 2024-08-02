fe1e0f07c02e9c3e75a1e708786925c6343aae63
Junius Pun <juniuspun00@gmail.com>
2024-08-02 18:12:51 -0400

    feat: update run scripts to use new function

--- run_mini.py
+++ run_mini.py
@@ -1,6 +1,8 @@
 import runs.kratos_mini as mini
 from structure.run import Runner
 from util.plot import plot_xy
+from util.results import save_and_plot
+from os.path import join
 
 runner = Runner()
 
@@ -13,10 +15,15 @@ results = runner.run_all_threaded(
     desc='run_mini',
     num_parallel_tasks=8,
     filter_params=mini.FILTER_PARAMS,
-    filter_results=mini.FILTER_RESULTS
+    filter_results=mini.FILTER_RESULTS,
+    allow_skipping=True,
+    avoid_mult=True
 )
 
-for root_dir, df in results.items():
-    filesafe_name = f"{root_dir.replace('/', '_')}_results"
-    plot_xy(df, ['lut_size'], 'ble_count', 'fmax', subplots_identifiers=['sparsity', 'data_width'], y_axis_col_secondary='clb', save_path=f"{filesafe_name}.png")
-    df.to_csv(f"{filesafe_name}.csv")
+def plot_fn(save_dir, filesafe_name, df):
+    save_path = join(save_dir, f"{filesafe_name}" + "_ble_{col}.png")
+    for col in ['fmax', 'clb']:
+        # plot_xy(df, ['sparsity'], 'lut_size', col, subplots_identifiers=['data_width'], save_path=save_path)
+        plot_xy(df, ['lut_size'], 'ble_count', col, subplots_identifiers=['sparsity', 'data_width'], save_path=save_path.format(col=col))
+
+save_and_plot(results, plot_fn)
