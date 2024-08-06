5454114a5a6a6be90ab0ab3549fd60d22ad70abf
Junius Pun <juniuspun00@gmail.com>
2024-08-06 18:16:02 -0400

    feat: add df mod functionality

--- run_kratos.py
+++ run_kratos.py
@@ -15,7 +15,8 @@ results = runner.run_all_threaded(
     desc='run_exploration',
     num_parallel_tasks=4,
     filter_params=kratos.FILTER_PARAMS,
-    filter_results=kratos.FILTER_RESULTS
+    filter_results=kratos.FILTER_RESULTS,
+    avoid_mult=True
 )
 
 def plot_fn(save_dir, filesafe_name, df):
@@ -24,4 +25,4 @@ def plot_fn(save_dir, filesafe_name, df):
         # plot_xy(df, ['sparsity'], 'lut_size', col, subplots_identifiers=['data_width'], save_path=save_path)
         plot_xy(df, ['lut_size'], 'ble_count', col, subplots_identifiers=['sparsity', 'data_width'], save_path=save_path.format(col=col))
 
-save_and_plot(results, plot_fn)
+save_and_plot(results, plot_fn=plot_fn)
--- run_mini.py
+++ run_mini.py
@@ -8,7 +8,11 @@ runner = Runner()
 
 # add experiments
 # mini.explore_conv_1d_fu(runner) # mini conv1d-FU
-mini.explore_conv_1d_pw(runner) # mini conv1d-PW
+# mini.explore_conv_1d_pw(runner) # mini conv1d-PW
+# mini.explore_conv_2d_fu(runner) # mini conv2d-FU
+# mini.explore_conv_2d_rp(runner) # mini conv2d-RP
+# mini.explore_conv_2d_pw(runner) # mini conv2d-PW
+# mini.explore_gemmt_fu(runner) # mini gemmt-FU
 # ...and others
 
 results = runner.run_all_threaded(
@@ -20,10 +24,14 @@ results = runner.run_all_threaded(
     avoid_mult=True
 )
 
+def df_mod_fn(df):
+    df['clb_avg_util'] = df.apply(lambda row: row['fle'] / row['clb'] / row['ble_count'], axis=1)
+    return df
+
 def plot_fn(save_dir, filesafe_name, df):
     save_path = join(save_dir, f"{filesafe_name}" + "_ble_{col}.png")
-    for col in ['fmax', 'clb']:
+    for col in ['fmax', 'clb', 'fle', 'clb_avg_util']:
         # plot_xy(df, ['sparsity'], 'lut_size', col, subplots_identifiers=['data_width'], save_path=save_path)
-        plot_xy(df, ['lut_size'], 'ble_count', col, subplots_identifiers=['sparsity', 'data_width'], save_path=save_path.format(col=col))
+        plot_xy(df, ['lut_size'], 'ble_count', col, subplots_identifiers=['CLB_groups_per_xb', 'sparsity', 'data_width'], save_path=save_path.format(col=col))
 
-save_and_plot(results, plot_fn)
+save_and_plot(results, df_mod_fn=df_mod_fn, plot_fn=plot_fn)
