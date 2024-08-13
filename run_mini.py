09154f3d69a7a8a6eda51135768ac68b4246c20f
Junius Pun <juniuspun00@gmail.com>
2024-08-13 17:15:53 -0400

    feat: normalization function; ADP flow for mini

--- run_mini.py
+++ run_mini.py
@@ -1,5 +1,6 @@
 import runs.kratos_mini as mini
 from structure.run import Runner
+from util.calc import normalize_df
 from util.plot import plot_xy
 from util.results import save_and_plot
 from os.path import join
@@ -25,13 +26,33 @@ results = runner.run_all_threaded(
 )
 
 def df_mod_fn(df):
+    # average CLB utilization
     df['clb_avg_util'] = df.apply(lambda row: row['fle'] / row['clb'] / row['ble_count'], axis=1)
+    return df
+    
+def grp_mod_fn(df):
+    # rough ADP
+    extracted = df[['cpd', 'clb', 'ble_count']].copy(deep=True)
+    extracted['total_area'] = extracted.apply(lambda row: row['clb'] * row['ble_count'], axis=1)
+    normalize_df(extracted, cols=['cpd', 'total_area'], min_max_vals=(1, 2))
+    df['adp'] = extracted.apply(lambda row: row['cpd'] * row['total_area'], axis=1)
+    normalize_df(df, norm_type='min-max', cols=['adp']) # normalize
     return df
 
 def plot_fn(save_dir, filesafe_name, df):
-    save_path = join(save_dir, f"{filesafe_name}" + "_ble_{col}.png")
-    for col in ['fmax', 'clb', 'fle', 'clb_avg_util']:
-        # plot_xy(df, ['sparsity'], 'lut_size', col, subplots_identifiers=['data_width'], save_path=save_path)
-        plot_xy(df, ['lut_size'], 'ble_count', col, subplots_identifiers=['CLB_groups_per_xb', 'sparsity', 'data_width'], save_path=save_path.format(col=col))
+    save_path = join(save_dir, f"{filesafe_name}" + "_{x_axis}_{col}.png")
+    short_labels = dict(lut_size='K', CLB_groups_per_xb='xb', sparsity='s', data_width='d', ble_count='N')
+    for col in ['adp', 'fmax', 'clb', 'clb_avg_util', 
+                #'fle', 'adder'
+    ]:
+        x_axis_list = ['ble_count', 'CLB_groups_per_xb']
+        # plot_xy(df, ['lut_size'], x_axis_list, col, subplots_identifiers=['sparsity', 'data_width'], save_path=save_path.format(x_axis='_'.join(x_axis_list), col=col), short_labels=short_labels)
+        for x_axis in x_axis_list:
+            plot_xy(df, ['lut_size'], x_axis, col, 
+                subplots_identifiers=[*[a for a in x_axis_list if a != x_axis], 'sparsity', 'data_width'], 
+                subplots_df_modifier=grp_mod_fn,
+                save_path=save_path.format(x_axis=x_axis, col=col), 
+                short_labels=short_labels
+            )
 
 save_and_plot(results, df_mod_fn=df_mod_fn, plot_fn=plot_fn)
--- util/calc.py
+++ util/calc.py
@@ -1,4 +1,6 @@
 import math
+from pandas import DataFrame
+from typing import Literal
 
 def get_portrait_square(n: int) -> tuple[int, int]:
     """
@@ -10,3 +12,30 @@ def get_portrait_square(n: int) -> tuple[int, int]:
 
     # favor portrait layout
     return max(side1, side2), min(side1, side2)
+
+def normalize_df(df: DataFrame, norm_type: Literal['min-max', 'mean'] = 'mean', cols: list[any] = None, min_max_vals: tuple[float, float] = None) -> None:
+    """
+    Normalizes specified columns of the DataFrame in-place.
+
+    Optional arguments:
+    * norm_type:'min-max'/'mean', type of normalization to apply.
+    * cols:list[any], list of columns to apply normalization to. If None, then entire DataFrame is normalized. Default: None
+    * min_max_vals:(min:float, max:float), minimum and maximum values to clamp to (i.e., normalization will be min-max). If None, then depends on norm_type. Default: None
+    """
+    extracted = df if cols is None else df[cols]
+    
+    if min_max_vals is not None:
+        min_val, max_val = min_max_vals
+        if min_val > max_val:
+            raise ValueError(f"Minimum value {min_val} provided is larger than maximum value {max_val}!")
+        
+        diff = max_val - min_val
+        extracted = min_val + diff * (extracted-extracted.min())/(extracted.max()-extracted.min())
+    elif norm_type == 'min-max':
+        extracted = (extracted-extracted.min())/(extracted.max()-extracted.min())
+    elif norm_type == 'mean':
+        extracted = (extracted-extracted.mean())/(extracted.std())
+    else:
+        raise ValueError("norm_type must be 'min-max' or 'mean'!")
+    
+    df.update(extracted)
