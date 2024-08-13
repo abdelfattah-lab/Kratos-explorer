5cf481dc40ed0ff1ff91e0817f558b9b29b17754
Junius Pun <juniuspun00@gmail.com>
2024-08-13 18:38:56 -0400

    chore: point to latest master branch

--- run_with_venv.sh
+++ run_with_venv.sh
@@ -3,9 +3,9 @@
 # This is intended to use a Python virtualenv (as opposed to conda or other virtual environments).
 
 # Change these parameters to your system.
-DEFAULT_VTR_ROOT="/media/samsdrive/vtr-exploration/vtr-verilog-to-routing/" # Default VTR path to use, if VTR_ROOT is not set.
-VENV_ROOT_PATH="/media/samsdrive/vtr-exploration/venv/"                     # Path to the virtual-env root.
-SCRIPT_FILE="run_mini.py"                                                        # change this to the Python script to run.
+DEFAULT_VTR_ROOT="/media/samsdrive/vtr-exploration/vtr-updated/"    # Default VTR path to use, if VTR_ROOT is not set.
+VENV_ROOT_PATH="/media/samsdrive/vtr-exploration/venv/"             # Path to the virtual-env root.
+SCRIPT_FILE="run_mini.py"                                           # change this to the Python script to run.
 
 if [ -z ${VTR_ROOT} ]; then
     export VTR_ROOT=$DEFAULT_VTR_ROOT
